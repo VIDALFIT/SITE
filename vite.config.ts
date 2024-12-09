a8d4d68314708ce0beba14c39f8606ed50a29748
francoisvid <vidalfrancois11@gmail.com>
2024-12-09 19:47:28 +0100

    Update VPS deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vidalfit-web/', // Ajout du slash final important
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
