b18bf3118de5014bc222f664712057ea08199dd7
francoisvid <vidalfrancois11@gmail.com>
2025-01-23 15:16:45 +0100

    google analytics done

--- src/utils/analytics.tsx
+++ src/utils/analytics.tsx
@@ -1,10 +1,7 @@
 export const trackAppDownload = (): void => {
     if (typeof window !== 'undefined' && window.gtag) {
-        console.log('trackAppDownload');
         window.gtag('event', 'conversion', {
             'send_to': 'AW-16831719447/2i1KCJm75I8aEJfQ_tk-'
         });
-    } else {
-        console.log('Google Analytics not loaded');
     }
 };
